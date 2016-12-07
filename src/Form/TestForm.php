7427e689cc28081cd1e5fb4fecc10a7ceb291148
Pavel <trobing1@gmail.com>
2016-12-07 17:56:44 +0600

    hook_mail, comments
    Change native php mail function to Drupal 8 hook_mail implementation;
    Add comments;

--- src/Form/TestForm.php
+++ src/Form/TestForm.php
@@ -1,15 +1,35 @@
 <?php
 
+/**
+ * @file
+ * Contains Drupal\test_form\Form\TestForm
+ */
+
 namespace Drupal\test_form\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+
+
+/**
+ * Inherits by Form API base class
+ * @see \Drupal\Core\Form\FormBase
+ */
+
 class TestForm extends FormBase {
+
+    /**
+     * Form name
+     */
 
     public function getFormId() {
         return 'test_form';
     }
+
+    /**
+     * Form structure
+     */
     
     public function  buildForm(array $form, FormStateInterface $form_state) {
         $form['full_name'] = array(
@@ -22,30 +42,34 @@ class TestForm extends FormBase {
             '#type' => 'number',
             '#title' => $this->t('Your age'),
             '#required' => 'true',
-            '#size' => 2
+            '#size' => 2,
         );
         
         $form['subject'] = array(
             '#type' => 'textfield',
             '#title' => $this->t('Subject'),
-            '#required' => 'true'
+            '#required' => 'true',
         );
         
         $form['message'] = array(
             '#type' => 'textarea',
             '#title' => $this->t('Message'),
-            '#required' => 'true'
+            '#required' => 'true',
         );
         
         $form['actions']['#type'] = 'actions';
         $form['actions']['submit'] = array(
             '#type' => 'submit',
             '#value' => $this->t('Send message'),
-            '#button_type' => 'primary'
+            '#button_type' => 'primary',
         );
         
         return $form;
     }
+
+    /**
+     * validate form
+     */
     
     public function validateForm(array &$form, FormStateInterface $form_state) {
         $name = $form_state->getValue('full_name');
@@ -89,20 +113,42 @@ class TestForm extends FormBase {
             $form_state->setErrorByName('message', $this->t('Too long message.'));
         }
     }
-    
+
+    /**
+     * Send email on submit
+     */
+
     public function submitForm(array &$form, FormStateInterface $form_state) {
         $to = \Drupal::config('system.site')->get('mail');
-        $subject = $form_state->getValue('subject');
-        $message = 'Name: ' . $form_state->getValue('full_name') . '<br>' .
-                'Age: ' . $form_state->getValue('age') . '<br>' .
-                'Message: ' . $form_state->getValue('message') . '<br>';
-        $result = mail($to, $subject, $message);
-        
-        if ($result)
+        $langcode = \Drupal::currentUser()->getPreferredLangcode();
+        $params = array(
+            'name' => $form_state->getValue('full_name'),
+            'age' => $form_state->getValue('age'),
+            'message' => $form_state->getValue('message'),
+            'subject' => $form_state->getValue('subject')
+        );
+
+        $message = \Drupal::service('plugin.manager.mail')->mail('test_form', 'contact', $to, $langcode, $params);  // looks not pretty, but that's from documentation example...
+
+        if ($message['result'])
             drupal_set_message($this->t('Form successfully sent!'));
         else
             drupal_set_message($this->t('Form did not send!!!!'));
     }
-    
+
+    /**
+     * Mail template
+     * Implements hook_mail
+     */
+
+    function test_form_mail($key, &$message, $params) {
+        switch ($key) {
+            case 'contact':
+                $message['subject'] = $params['subject'];
+                $message['body'][] = 'Name: ' . $params['name'] . '\n\n' .
+                    'Age: ' . $params['age'] . '\n\n' .
+                    'Message: ' . $params['message'] . '\n\n';
+        }
+    }
 }
 
